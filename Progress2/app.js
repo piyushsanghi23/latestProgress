fe7abf00eb2d9cc23c8c6218576f16fc11fbdac4
kavya gopireddy <gopireddykavya@yahoo.com>
2016-06-24 05:58:05 +0000

    App created.

---
+++ Progress2/app.js
@@ -0,0 +1,75 @@
+'use strict';
+
+(function() {
+    var app = {
+        data: {}
+    };
+
+    var bootstrap = function() {
+        $(function() {
+            app.mobileApp = new kendo.mobile.Application(document.body, {
+                skin: 'nova',
+                initial: 'components/homeView/view.html'
+            });
+        });
+    };
+
+    if (window.cordova) {
+        document.addEventListener('deviceready', function() {
+            if (navigator && navigator.splashscreen) {
+                navigator.splashscreen.hide();
+            }
+
+            var element = document.getElementById('appDrawer');
+            if (typeof(element) != 'undefined' && element !== null) {
+                if (window.navigator.msPointerEnabled) {
+                    $('#navigation-container').on('MSPointerDown', 'a', function(event) {
+                        app.keepActiveState($(this));
+                    });
+                } else {
+                    $('#navigation-container').on('touchstart', 'a', function(event) {
+                        app.keepActiveState($(this).closest('li'));
+                    });
+                }
+            }
+
+            bootstrap();
+        }, false);
+    } else {
+        bootstrap();
+    }
+
+    app.keepActiveState = function _keepActiveState(item) {
+        var currentItem = item;
+        $('#navigation-container li.active').removeClass('active');
+        currentItem.addClass('active');
+    };
+
+    window.app = app;
+
+    app.isOnline = function() {
+        if (!navigator || !navigator.connection) {
+            return true;
+        } else {
+            return navigator.connection.type !== 'none';
+        }
+    };
+
+    app.openLink = function(url) {
+        if (url.substring(0, 4) === 'geo:' && device.platform === 'iOS') {
+            url = 'http://maps.apple.com/?ll=' + url.substring(4, url.length);
+        }
+
+        window.open(url, '_system');
+        if (window.event) {
+            window.event.preventDefault && window.event.preventDefault();
+            window.event.returnValue = false;
+        }
+    };
+
+}());
+
+// START_CUSTOM_CODE_kendoUiMobileApp
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_kendoUiMobileApp

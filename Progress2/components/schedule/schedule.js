b83aa7329c73beb11daa17f5bd7f0656ae538a34
kavya gopireddy <varshagopireddy@gmail.com>
2016-08-01 10:21:41 +0000

    Changes made by kavya gopireddy (varshagopireddy@gmail.com). schedule.css, bleListener.js and globalVariables

--- Progress2/components/schedule/schedule.js
+++ Progress2/components/schedule/schedule.js
@@ -141,28 +141,40 @@ function dis() {
         var node = document.createElement("li");
         node.setAttribute("class", "listItem");
         node.setAttribute('id', i);
+        node.setAttribute('onclick', 'show(this.id)');
         var text = emp[i].round;
         var textnode = document.createTextNode(text);
         node.appendChild(textnode);
         document.getElementById("employee_list").appendChild(node);
-        
-      /*  var node2=document.createElement('h3');
-        node2.setAttribute("class", "timeItem");
-        var text1 = emp[i].time;
-        var textnode1 = document.createTextNode(text1);
-        node2.appendChild(textnode1);
-        document.getElementById(i).appendChild(node2);*/
-        
+
+         var node2=document.createElement('div');
+          node2.setAttribute("class", "arrow");
+        node2.setAttribute('id','arrow'+i);
+          var text1 = '\u276f';
+          var textnode1 = document.createTextNode(text1);
+          node2.appendChild(textnode1);
+          document.getElementById(i).appendChild(node2);
+
         var div = document.createElement("div");
         div.setAttribute("class", "divItem");
         div.setAttribute('id', 'div' + i);
-        var img=document.createElement('img');
-         //div.setAttribute("class", "imgItem");
-        img.setAttribute('src',emp[i].image);
+        var img = document.createElement('img');
+        img.setAttribute("class", "imgItem");
+        img.setAttribute('src', emp[i].image);
         div.appendChild(img);
-        var text2 = emp[i].name + "\n" + emp[i].role;
+        var name= document.createElement("div");
+        name.setAttribute('id','name');
+        var text2 = emp[i].name ;
         var textnode2 = document.createTextNode(text2);
-        div.appendChild(textnode2);
+         name.appendChild(textnode2);
+        //document.getElementById("employee_list").appendChild(div);
+         var role= document.createElement("div");
+        role.setAttribute('id','role');
+        var text3 = emp[i].role ;
+        var textnode3 = document.createTextNode(text3);
+         role.appendChild(textnode3);
+         div.appendChild(name);
+        div.appendChild(role);
         document.getElementById("employee_list").appendChild(div);
         if (i == emp.length - 1)
             clearInterval(my);
@@ -171,25 +183,28 @@ function dis() {
 }
 var my = setInterval('dis()', 1000);
 
-function show() {
-    // if(document.getElementByClassName=='listItem')
-
-    document.getElementById('employee_list').onclick = function (e) {
-        //alert(e.target.id)
-        var x = 'div' + e.target.id;
-        for (i in emp) {
-            if (x == 'div' + i) {
-               //alert(x + "   " + i);
-                if(document.getElementById('div' + i).style.display =='none')
-                document.getElementById('div' + i).style.display = 'block';
-                else
-                    document.getElementById('div' + i).style.display = 'none';
-            } else {
-                //alert(x + "   " + i);
+function show(id) {
+    var x = 'div' + id;
+    //alert(emp.length);
+    for (i in emp) {
+        if (x == 'div' + i) {
+            //alert(x + "   " + i);
+            if (document.getElementById('div' + i).style.display == 'block')
+                {
+                    var arrow=document.getElementById('arrow' + i);
+                    arrow.setAttribute('class','arrow');
                 document.getElementById('div' + i).style.display = 'none';
-            }
+                }
+            else
+                {
+                document.getElementById('div' + i).style.display = 'block';
+                var arrow=document.getElementById('arrow' + i);
+                    arrow.setAttribute('class','arrow2');}
+        } else {
+            //alert(x + "   " + i);
+            document.getElementById('div' + i).style.display = 'none';
+            var arrow=document.getElementById('arrow' + i);
+                    arrow.setAttribute('class','arrow');
         }
-        // document.getElementById(x).style.display='block';
-
     }
 }
